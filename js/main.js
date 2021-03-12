b038e8c2b4687829efda3371f27138051434343d
Bohdan <dogdanlashko@gmail.com>
2021-03-12 13:48:56 +0200

    ✨

--- js/main.js
+++ js/main.js
@@ -13,14 +13,25 @@ function HEXToVBColor(hex) {
 
 function toggleTheme() {
     body.className = (body.className == 'th1') ? 'th2' : 'th1'
-    cube.material.color.setHex(
-        HEXToVBColor(getComputedStyle(body).getPropertyValue('--el'))
+    scene.children.forEach(obj =>
+        obj.material.color.setHex(
+            HEXToVBColor(getComputedStyle(body).getPropertyValue('--el')))
     )
 }
 
-function helpMenu() {
-    help.style.display = (help.style.display == 'block') ? 'none' : 'block'
+document.getElementById('theme_button').onclick = () => {
+    body.className = (body.className == 'th1') ? 'th2' : 'th1'
+    ambt.color.setHex(HEXToVBColor(getComputedStyle(body).getPropertyValue('--el')));
+    scene.children.forEach(obj =>
+        obj.material.color.setHex(
+            HEXToVBColor(getComputedStyle(body).getPropertyValue('--el')))
+    )
 }
+
+document.getElementById('help_button').onclick = () =>
+    help.style.display = (help.style.display == 'block') ? 'none' : 'block'
+
+
 
 // THREE
 
@@ -44,19 +55,27 @@ controls.maxPolarAngle = Math.PI / 2;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 
 
 renderer.render(scene, camera);
 
-const animate = function() {
+var spotLight = new THREE.SpotLight(0xaaaaaa);
+scene.add(spotLight);
+
+// var angle = 0;
+
+const animate = function () {
     requestAnimationFrame(animate);
     controls.update();
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+
+    // angle -= 0.05;
+
+    spotLight.position.copy(camera.position);
+
+    // spotLight.position.x = 500 * Math.cos(angle);
+    // spotLight.position.y = spotLight.position.x;
+    // spotLight.position.z = 500 * Math.sin(angle);
 
     renderer.render(scene, camera);
 };
@@ -65,15 +84,29 @@ animate();
 
 const gltfLoader = new THREE.GLTFLoader();
 
-gltfLoader.load('./obj/Skeleton.glb', function(gltf) {
-    console.log(gltf.scene)
+gltfLoader.load('./obj/Skeleton.glb', function (gltf) {
+    // console.log(gltf.scene)
+    gltf.scene.traverse(child => {
+        if (child.material) child.material.metalness = 0
+    })
 
-    scene.add(gltf.scene);
+    scene.add(gltf.scene)
 
     //scene.scale.set(10, 10, 10)
 
-}, undefined, function(error) {
+}, undefined, function (error) {
 
-    console.error(error);
+    console.error(error)
 
-});
+})
+
+const light = new THREE.PointLight(0xffffff, 0.3, 1000);
+light.position.set(300, 500, 100);
+
+scene.add(light);
+
+
+const ambt = new THREE.AmbientLight(0xffffff, 0.1); // soft white light
+scene.add(ambt);
+
+console.log('scene0', scene.children)
